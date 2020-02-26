20dd4545bb292e41d5b2b9dcb7d4c01f14c95e2a
irakravchuk <52664019+irakravchuk@users.noreply.github.com>
2020-02-26 15:02:05 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = var.region
+}
+
+data "null_data_source" "values" {
+  inputs = {
+    vpc_id = var.vpc_id
+    }
+}
+
+resource "aws_instance" "scalr" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet
+  vpc_security_group_ids = var.sg
+  key_name               = var.key
+  count                  = var.instance_count
+}
